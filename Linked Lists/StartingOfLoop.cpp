046ec673170a8967442daf7443050898ace26565
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-10-07 00:08:28 +0530

    done

--- Linked Lists/StartingOfLoop.cpp
+++ Linked Lists/StartingOfLoop.cpp
@@ -1,5 +1,5 @@
-// C++ program to return first node of loop.
-#include <bits/stdc++.h>
+
+#include <iostream.h>
 using namespace std;
 
 struct Node {
@@ -15,7 +15,7 @@ Node* newNode(int key)
 	return temp;
 }
 
-// A utility function to print a linked list
+
 void printList(Node* head)
 {
 	while (head != NULL) {
@@ -25,24 +25,18 @@ void printList(Node* head)
 	cout << endl;
 }
 
-// Function to detect and remove loop
-// in a linked list that may contain loop
 Node* detectAndRemoveLoop(Node* head)
 {
-	// If list is empty or has only one node
-	// without loop
+	
 	if (head == NULL || head->next == NULL)
 		return NULL;
 
 	Node *slow = head, *fast = head;
 
-	// Move slow and fast 1 and 2 steps
-	// ahead respectively.
+	
 	slow = slow->next;
 	fast = fast->next->next;
 
-	// Search for loop using slow and
-	// fast pointers
 	while (fast && fast->next) {
 		if (slow == fast)
 			break;
@@ -50,12 +44,10 @@ Node* detectAndRemoveLoop(Node* head)
 		fast = fast->next->next;
 	}
 
-	// If loop does not exist
+	
 	if (slow != fast)
 		return NULL;
 
-	// If loop exists. Start slow from
-	// head and fast from meeting point.
 	slow = head;
 	while (slow != fast) {
 		slow = slow->next;
@@ -65,7 +57,7 @@ Node* detectAndRemoveLoop(Node* head)
 	return slow;
 }
 
-/* Driver program to test above function*/
+
 int main()
 {
 	Node* head = newNode(50);
@@ -74,7 +66,7 @@ int main()
 	head->next->next->next = newNode(4);
 	head->next->next->next->next = newNode(10);
 
-	/* Create a loop for testing */
+
 	head->next->next->next->next->next = head->next->next;
 
 	Node* res = detectAndRemoveLoop(head);
