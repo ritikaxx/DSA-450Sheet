de08e8c1854e9847f9351fd8938bd0f990b4755d
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-10-12 22:38:28 +0530

    palindrome

---
+++ Linked Lists/Palindrome.cpp
@@ -0,0 +1,28 @@
+bool Palindrome(Node* head){
+    Node* slow=head;
+    Node* fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+}
+
+Node* prev =NULL;
+Node* next;
+Node* curr=slow;
+
+while(curr!=NULL){
+    next=curr->next;
+    curr->next=prev;
+    prev=curr;
+    curr=next;
+}
+
+fast=head;
+while(prev!=NULL){
+    if(fast->data!=prev->data){
+        return false;
+    }
+    fast=fast->next;
+    prev=prev->next;
+}
