ed1bd4e4753a6adb02ebff630635ef6abb001ce7
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-10-12 22:28:41 +0530

    split list

---
+++ Linked Lists/SplitList.cpp
@@ -0,0 +1,16 @@
+Node* Split(Node* head,Node **head1_ref,Node **head2_ref){
+    Node* slow=head;
+    Node* fast= head;
+    while(fast!=head && fast->next!=head){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    *head1_ref = head;
+    *head2_ref= slow->next;
+    slow->next= *head1_ref;
+    Node* curr= *head2_ref;
+    while(curr->next!=head){
+        curr=curr->next;
+    }
+    curr->next=*head2_ref;
+}
